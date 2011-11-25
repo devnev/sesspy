6cbc8cc4477745150359ecb14fc64cc1394aa05d
Mark Nevill <mark.nevill@gmail.com>
2011-11-25 01:35:51 +0100

    Added test runner script.

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ x"$1" = x"-c" ]
+then
+	coverage erase
+	for testpy in tests/test_*.py
+	do
+		coverage run --append $testpy
+	done
+	coverage html --include='sesspy/*'
+else
+	find tests -name '*.py' | tr / . | sed 's/\.py$//' | xargs python -m unittest
+fi
