43f8f7e73a230976defbd8c136113468627137eb
Mark Nevill <mark.nevill@gmail.com>
2011-11-26 14:05:52 +0100

    Added config and sqlalchemy helper modules. (untested)

---
+++ sesspy/config.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Mark Nevill
+# This file is part of sesspy.
+# 
+# sesspy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# sesspy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with sesspy.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+class FeatureUnconfigured(Exception):
+    pass
+
+class LazyConfigReader(object):
+    def __init__(self, config, paths, expanduser=True, leaf=None):
+        self.config = config
+        self.paths = paths
+        self.expanduser = expanduser
+        self.leaf = leaf
+        self.load_lock = threading.Lock()
+        self.read_paths = None
+
+    def read(self):
+        import os, os.path
+        paths = self.paths
+        if self.leaf:
+            paths = [os.path.join(p, self.leaf) for p in paths]
+        if self.expanduser:
+            paths = [os.path.expanduser(p) for p in paths]
+        return self.config.read(paths)
+
+    def __call__(self):
+        if self.read_paths is not None:
+            return self.config
+        with self.load_lock:
+            if self.read_paths is not None:
+                return self.config
+            self.read_paths = self.read()
+            return self.config
+
+ConfigOption(object):
+    def __init__(self, get_config, section, option):
+        self.get_config = get_config
+        self.section, self.option = section, option
+
+    def __call__(self):
+        config = self.get_config()
+        try:
+            return config.get(self.section, self.option)
+        except (configparser.NoSectionError,
+                configparser.NoOptionError) as e:
+            raise FeatureUnconfigured(str(e))
---
+++ sesspy/sqlalchemy.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Mark Nevill
+# This file is part of sesspy.
+# 
+# sesspy is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# sesspy is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with sesspy.  If not, see <http://www.gnu.org/licenses/>.
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+class ConnectionFactory(object):
+
+    def __init__(self, db_uri, **kwargs):
+        self.db_uri = db_uri
+        self.engine_kwargs = kwargs
+
+    def __call__(self):
+        db_uri = self.db_uri
+        if callable(db_uri):
+            db_uri = db_uri()
+
+        engine = sqlalchemy.create_engine(db_uri, **self.engine_kwargs)
+        return engine
+
+def db_connection(db_uri, engine_args=None, name=None, registry=None, noretry_exceptions=None):
+    from . import session
+
+    component = session.SingletonFactory(
+        factory=ConnectionFactory(db_uri, **(engine_kwargs or {})),
+        instance_opener=SingletonOpener,
+        local_openers=False,
+        noretry_exceptions=noretry_exceptions,
+    )
+
+    if name:
+        if registry is None:
+            from .registry import default_registry
+            registry = default_registry
+        registry.register_component(name, component)
+
+    return component
+
+class ORMSessionFactory(object):
+
+    def __init__(self, connection, session_args=None):
+        session_args = session_args or {}
+        session_args['bind'] = connection
+        self.session_maker = sessionmaker(**session_args)
+
+    def open(self):
+        return self.session_maker()
+
+    def commit(self, session):
+        session.commit()
+
+    def abort(self, session):
+        session.rollback()
+
+def orm_session(db_uri, engine_kwargs=None, name=None, registry=None, noretry_exceptions=None):
+    from . import session
+
+    component = session.SingletonFactory(
+        factory=ConnectionFactory(db_uri, **engine_kwargs),
+        instance_opener=ORMSessionFactory,
+        noretry_exceptions=noretry_exceptions,
+    )
+
+    if name:
+        if registry is None:
+            from .registry import default_registry
+            registry = default_registry
+        registry.register_component(name, component)
+
+    return component
+
+def orm_counting_session(db_uri, engine_kwargs=None, counting_opener=None, name=None, registry=None, noretry_exceptions=None):
+    from . import session, openers
+    if counting_opener is None:
+        counting_opener = openers.CountingOpener
+
+    component = session.SingletonFactory(
+        factory=ConnectionFactory(db_uri, **engine_kwargs),
+        instance_opener=openers.combine_openers(
+            ORMSessionFactory,
+            counting_opener,
+        ),
+        noretry_exceptions=noretry_exceptions,
+    )
+
+    if name:
+        if registry is None:
+            from .registry import default_registry
+            registry = default_registry
+        registry.register_component(name, component)
+
+    return component
+
