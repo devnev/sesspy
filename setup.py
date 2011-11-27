e2ad10f017cc16505f5421a0a6870574d0350acc
Mark Nevill <mark@nevill.ch>
2011-11-27 16:29:17 +0100

    Extended setup.py to include licensing and author information and description.

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
 setup(
-      name = "sesspy",
-      version = "0.1.0",
-      packages = find_packages(exclude=['tests']),
-      install_requires = [],
+    name = "sesspy",
+    version = "0.1.0",
+    packages = find_packages(exclude=['tests']),
+    install_requires = [],
+    extras_require = {
+        "sqlalchemy": ["sqlalchemy"],
+    },
+    package_data = {
+        '': ['/COPYING', '/COPYING.LESSER'],
+    },
+    include_package_data=True,
+
+    test_suite = "tests",
+    tests_require = ["mock", "sqlalchemy"],
+
+    author = "Mark Nevill",
+    author_email = "mark@nevill.ch",
+    description = "Session/Transaction Management and Dependency Injection",
+    license = "LGPLv3",
+    keywords = "session transaction dependency injection",
 )
