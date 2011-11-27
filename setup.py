0fdd4e58e40bd2097718426a45c71f1a376a853c
Mark Nevill <mark@nevill.ch>
2011-11-27 15:47:48 +0100

    Exclude tests directory from packages in setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 setup(
       name = "sesspy",
       version = "0.1.0",
-      packages = find_packages(),
+      packages = find_packages(exclude=['tests']),
       install_requires = [],
 )
