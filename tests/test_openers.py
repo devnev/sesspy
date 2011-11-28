f3cd066a2f640a056449fc85b8784d4ebbee8233
Mark Nevill <mark@nevill.ch>
2011-11-28 10:18:41 +0100

    Python 2.5 compatibility fix in openers tests.

--- tests/test_openers.py
+++ tests/test_openers.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with sesspy.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import with_statement
+
 if __name__ == '__main__':
     import sys
     import os, os.path
