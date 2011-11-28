31647ad7829637d590a79fe85c54bf16d7b6c5f2
Mark Nevill <mark@nevill.ch>
2011-11-28 10:29:17 +0100

    Release 0.2.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "sesspy",
-    version = "0.2.0",
+    version = "0.2.1",
     packages = find_packages(exclude=['tests']),
     install_requires = [],
     extras_require = {
