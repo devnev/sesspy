8b26bfcca53293a882a63535b227993d9e4f5b3f
Mark Nevill <mark.nevill@gmail.com>
2012-03-23 11:00:10 +0100

    Added package url to setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name = "sesspy",
     version = "0.2.3",
+    url = "http://nevill.ch/sesspy/",
+
     packages = find_packages(exclude=['tests']),
     install_requires = [],
     extras_require = {
